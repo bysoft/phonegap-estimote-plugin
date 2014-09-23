9b84b468cd8cd161ebdc9ba2f3e945c49d2df62f
Anthony Rumsey <arumsey@adobe.com>
2014-09-22 21:08:13 -0400

    - changed region to support any UUID

--- src/android/EstimotePlugin.java
+++ src/android/EstimotePlugin.java
@@ -25,7 +25,7 @@ public class EstimotePlugin extends CordovaPlugin
 {
 
     private static final String ESTIMOTE_PROXIMITY_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
-    private static final Region ALL_ESTIMOTE_BEACONS = new Region("regionId", ESTIMOTE_PROXIMITY_UUID, null, null);
+    private static final Region ALL_ESTIMOTE_BEACONS = new Region("regionId", null, null, null);
 
     private static final String LOG_TAG					= "EstimotePlugin";
 	
